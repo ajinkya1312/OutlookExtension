359662898cb9852d41c38a11d79be89b1809fbb0
Ajinkya Patil <ajinkya.patil@soroco.com>
2022-04-21 06:51:18 +0530

    Linq initial commit

---
+++ Linq/LinqWorkshop/LinqWorkshop/Event.cs
@@ -0,0 +1,58 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWorkshop
+{
+    [Table("events")]
+    public class Event
+    {
+        [Column("number")]
+        public int Number { get; set; }
+        [Column("time")]
+        public DateTime Time { get; set; }
+        [Column("trimmed_exe")]
+        public string TrimmedExe { get; set; }
+        [Column("domain")]
+        public string Domain { get; set; }
+        [Column("typing")]
+        public bool Typing { get; set; }
+        [Column("channel")]
+        public string Channel { get; set; }
+        [Column("context")]
+        public string Context { get; set; }
+        [Column("element_name")]
+        public string ElementName { get; set; }
+        [Column("element_type")]
+        public string ElementType { get; set; }
+        [Column("product_version")]
+        public string ProductVersion { get; set; }
+        [Column("application_context")]
+        public string ApplicationContext { get; set; }
+        [Column("missing_context_reason")]
+        public string MissingContextReason { get; set; }
+        [Column("missing_application_context_reason")]
+        public string MissingApplicationContextReason { get; set; }
+        [Column("extra_fields")]
+        public string ExtraFields { get; set; }
+        [Column("product_name")]
+        public string ProductName { get; set; }
+        [Column("sap_connection_string")]
+        public string SapConnectionString { get; set; }
+        [Column("sap_username")]
+        public string SapUsername { get; set; }
+        [Column("regex_replace_count")]
+        public int RegexReplaceCount { get; set; }
+        [Column("type_of_action")]
+        public string TypeOfAction { get; set; }
+        [Column("data_agent_version")]
+        public string DataAgentVersion { get; set; }
+        [Column("extension_version")]
+        public string ExtensionVersion { get; set; }
+        [Column("interval_s")]
+        public double Interval_s { get; set; }
+    }
+}
---
+++ Linq/LinqWorkshop/LinqWorkshop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+namespace LinqWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var session = new SQLiteConnection("..\\..\\..\\event_data.db", false);
+            var events = session.Query<Event>("select * from events").ToList();
+
+            var event_count = events.Count();
+            Console.WriteLine("Number of events : " + event_count);
+
+            // 1. select clause
+
+            // 2. select where clause
+
+            // 3. order by / orderby descending
+           
+            // 4. Reverse
+
+            // 5. First or default 
+
+            // 6. Sum / Min / Max / Average
+
+            // 7. take / skip
+
+            // 8. group by
+
+
+
+#if DEBUG
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+#endif
+
+
+        }
+    }
+}
